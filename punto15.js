6dcc66fa82901dab40cd038d67f4392c2a5ed22f
josrarba <161080749+josrarba@users.noreply.github.com>
2024-10-23 19:48:01 -0500

    punto 15 organizado

--- punto15.js
+++ punto15.js
@@ -1,14 +1,13 @@
-let a = 5 
-let b = 2
-let c = 9
+let a = 3;
+let b = 7;
+let c = 2;
 
-let x1
-let x2
+let x1;
+let x2;
 
-let menorb = b*-1
+let minusB = b * -1;
+x1 = (minusB - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+x2 = (minusB + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
 
-x1 = (menorb - Math.sqrt(b**2 - 4*a*c)) / (2*a)
-x2 = x1 = (menorb + Math.sqrt(b**2 - 4*a*c)) / (2*a)
-
-console.log(`el primer resultado (x1) es: ${x1}`)
-console.log(`el primer resultado (x1) es: ${x2}`)
+console.log(`x1 = ${x1}`);
+console.log(`x2 = ${x2}`);
